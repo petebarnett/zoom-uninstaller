e14f7e1d0b4fcf5b5fa4a8fa9753b1f8d5201730
Pete Barnett <pete.barnett@commercelabs.io>
2019-05-10 14:03:41 +0100

    Initial commit

---
+++ uninstall-zoom.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+if [[ $UID != 0 ]]; then
+    echo "Please run this script with sudo:"
+    echo "sudo $0 $*"
+    exit 1
+fi
+echo "Stopping running Zoom processes"
+killall -9 zoom.us
+killall -9 ZoomOpener
+echo "Removing main application"
+rm -rf /Applications/zoom.us.app
+echo "Removing application support"
+rm -rf ~/Library/Application\ Support/zoom.us
+echo "Removing application caches"
+rm -rf ~/Library/Caches/us.zoom.xos
+echo "Removing logs"
+rm -rf ~/Library/Logs/zoom.us
+echo "Removing preferences"
+rm ~/Library/Preferences/us.zoom.xos.plist
